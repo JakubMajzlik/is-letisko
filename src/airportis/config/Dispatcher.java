b48a5260e2deeac9dbc457d4826d8a441d70ccc3
Jakub Majzlik <jakubmajzlik@gmail.com>
2018-10-10 22:04:30 +0200

    Add some basic configs + HelloWorld

---
+++ src/airportis/config/Dispatcher.java
@@ -0,0 +1,26 @@
+package airportis.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Dispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		System.out.println("WebConf");
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/airportis/config/WebConfig.java
@@ -0,0 +1,99 @@
+package airportis.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="airportis.app")
+@EnableTransactionManagement
+@PropertySource("classpath:/resources/db.properties")
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired 
+	private Environment enviroment;
+
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		System.out.println("Creating viewResolver");
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource securityDataSource() {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		try {
+			dataSource.setDriverClass(enviroment.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		dataSource.setJdbcUrl(enviroment.getProperty("jdbc.url"));
+		dataSource.setUser(enviroment.getProperty("jdbc.user"));
+		dataSource.setPassword(enviroment.getProperty("jdbc.password"));
+		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory(){
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(securityDataSource());
+		sessionFactory.setPackagesToScan(enviroment.getProperty("hibernate.packageToScan"));
+		sessionFactory.setHibernateProperties(getHibernateProperties());		
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		return txManager;
+	}
+	
+	
+	private int getIntProperty(String propertyName) {
+		return Integer.parseInt(enviroment.getProperty(propertyName));
+	}
+	
+	private Properties getHibernateProperties() {
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", enviroment.getProperty("hibernate.dialect"));
+		props.setProperty("hibernate.show_sql", enviroment.getProperty("hibernate.show_sql"));	
+		return props;				
+	}
+
+	
+}
