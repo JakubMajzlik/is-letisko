45480c834876d34133cd60b77dba941356670db8
Jakub Majzlik <jakubmajzlik@gmail.com>
2018-10-11 09:56:39 +0200

    Fixed wrong implementation of interface in UserService

--- src/airportis/app/service/UserService.java
+++ src/airportis/app/service/UserService.java
@@ -1,7 +1,7 @@
 package airportis.app.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService extends UserDetails {
+public interface UserService extends UserDetailsService {
 
 }
--- src/airportis/app/service/UserServiceImpl.java
+++ src/airportis/app/service/UserServiceImpl.java
@@ -1,5 +1,14 @@
 package airportis.app.service;
 
-public class UserServiceImpl {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
