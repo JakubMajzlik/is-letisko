f6535db5f847045fb2cf76a465e4668d66baa1d2
Jakub Majzlik <jakubmajzlik@gmail.com>
2018-10-21 14:30:48 +0200

    Add user registration model without validations

---
+++ src/airportis/app/model/UserRegisterModel.java
@@ -0,0 +1,125 @@
+package airportis.app.model;
+
+public class UserRegisterModel {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String password1;
+	
+	private String password2;
+	
+	private String city;
+	
+	private String street;
+	
+	private String houseNumber;
+	
+	private String zip;
+	
+	private String country;
+	
+	private String identification_number;
+	
+	private String phoneNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword1() {
+		return password1;
+	}
+
+	public void setPassword1(String password1) {
+		this.password1 = password1;
+	}
+
+	public String getPassword2() {
+		return password2;
+	}
+
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getIdentification_number() {
+		return identification_number;
+	}
+
+	public void setIdentification_number(String identification_number) {
+		this.identification_number = identification_number;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
